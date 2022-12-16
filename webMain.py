9202070f5109fbe6fab8ef8fa5931276e2563fc3
Febrian Chrissma Bagaskara <91533059+FebrianChrissmaBagaskara@users.noreply.github.com>
2022-12-17 03:18:00 +0800

    Update webMain.py

--- webMain.py
+++ webMain.py
@@ -4,14 +4,14 @@ from secrets import choice
 import streamlit as st
 
 #opencv library
-from opencv import face_recognition
-from datetime import datetime
-from PIL import Image
+# from opencv import face_recognition
+# from datetime import datetime
+# from PIL import Image
 import pandas as pd
-import numpy as np
-import cv2
-import os
-import time
+# import numpy as np
+# import cv2
+# import os
+# import time
 
 
 FRAME_WINDOW = st.image([]) #frame window
